275f55f2752757a9d77a4852637b75c3f83582b6
ex3mpl <78493959+ex3mpl@users.noreply.github.com>
2024-10-22 23:31:49 +0500

    Task 2.1.4 completed
    * Added annotations Component and Autowiring in all class of model package
    * Added Qualifier annotation to Ocean class
    * Added constructors with Autowiring and fields
    * Added toString methods to classes where it needs

---
+++ src/main/java/koschei/models/Ocean1.java
@@ -0,0 +1,21 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Ocean1 {
+
+    private final Island2 island;
+
+    @Autowired
+    public Ocean1(@Qualifier("getIsland") Island2 island) {
+        this.island = island;
+    }
+
+    @Override
+    public String toString() {
+        return "на океане остров " + island.toString();
+    }
+}
